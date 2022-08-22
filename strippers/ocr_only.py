70968c71db0c9d78442c0964983fd18e77766dbc
Neel Shah <neelshah.sa@gmail.com>
2022-08-22 14:56:56 +0200

    OcrOnly algo (#3)

--- strippers/ocr_only.py
+++ strippers/ocr_only.py
@@ -10,5 +10,20 @@ class OcrOnlyStripper:
         self.output_img = self.input_img.copy()
 
     def strip(self):
+        boxes = pytesseract.image_to_data(self.input_img, output_type="dict")
+
+        for i in range(len(boxes["level"])):
+            # level mapping: 1 - page, 2 - block, 3 - paragraph, 4 - line, 5 - word
+            # take only words
+            if boxes["level"][i] != 5:
+                continue
+
+            x = boxes["left"][i]
+            y = boxes["top"][i]
+            w = boxes["width"][i]
+            h = boxes["height"][i]
+
+            cv2.rectangle(self.output_img, (x, y), (x + w, y + h), (0, 0, 0), -1)
+
         cv2.imwrite(self.output, self.output_img)
 
