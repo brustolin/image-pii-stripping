68f93396ee67808ded6146e56ae88a46f1a90e54
Neel Shah <neelshah.sa@gmail.com>
2022-08-22 17:18:42 +0200

    PII let's goooo

--- pii_stripper/ocr_only.py
+++ pii_stripper/ocr_only.py
@@ -1,6 +1,8 @@
 import cv2
 import pytesseract
 
+from .pii import is_pii
+
 
 class OcrOnlyStripper:
     def __init__(self, input, output=None):
@@ -19,15 +21,27 @@ class OcrOnlyStripper:
                 continue
 
             # filter empty text
-            if not boxes["text"][i].strip():
+            text = boxes["text"][i].strip()
+            if not text:
                 continue
 
-            x = boxes["left"][i]
-            y = boxes["top"][i]
-            w = boxes["width"][i]
-            h = boxes["height"][i]
+            # find other words in same line
+            line_words = [v for j, v in enumerate(boxes["text"]) if boxes["level"][j] == 5 and
+                    boxes["page_num"][j] == boxes["page_num"][j] and
+                    boxes["block_num"][j] == boxes["block_num"][i] and
+                    boxes["par_num"][j] == boxes["par_num"][i] and
+                    boxes["line_num"][j] == boxes["line_num"][i]]
 
-            cv2.rectangle(self.output_img, (x, y), (x + w, y + h), (0, 0, 0), -1)
+            line_text = "".join(line_words)
+
+            # draw rectangles over pii
+            # we check both the word and the reconstructed line that word belongs to for pii
+            if is_pii(text) or is_pii(line_text):
+                x = boxes["left"][i]
+                y = boxes["top"][i]
+                w = boxes["width"][i]
+                h = boxes["height"][i]
+
+                cv2.rectangle(self.output_img, (x, y), (x + w, y + h), (0, 0, 0), -1)
 
         cv2.imwrite(self.output, self.output_img)
-
